851c0685fd4aefb7740517a5b214f04c8e23459d
Sergio <sbtk87@hotmail.com>
2020-02-04 22:30:14 -0500

    Proyecto inicial

---
+++ src/main/java/com/klaus/app/KlausLeatherApplication.java
@@ -0,0 +1,13 @@
+package com.klaus.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlausLeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlausLeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klaus/app/KlausLeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klaus.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlausLeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
